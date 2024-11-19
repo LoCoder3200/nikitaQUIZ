a009e538d56c2e7a18d09bfe87735c319ed2f707
iderisadev <ider.kawabata@gmail.com>
2024-11-19 13:45:41 -0800

    nikita and lorenzo

--- webapp.py
+++ webapp.py
@@ -11,54 +11,19 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
 @app.route("/")
 def render_main():
     return render_template('index.html')
-
 @app.route("/page2")
-def render_page2():
-    session["start_time"] = time.time()
-
-    return render_template('question1.html')
-
+def render_pageTwo():
+    return render_template('page2.html')
+@app.route("/question2",methods=['GET','POST'])
+def render_pageThree():
+    if "answer1" not in session:
+        session["answer1"]=request.form['questionONE']
+    return render_template('page3.html')
 @app.route('/startOver')
 def startOver():
     session.clear() #clears variable values and creates a new session
     return redirect('/') # url_for('renderMain') could be replaced with '/'
-@app.route("/answersOne",methods=['GET','POST'])
-def page3():
-    if "answer1" not in session:
-        session["answer1"]=request.form['ques1']
-    return render_template('question2.html')
-@app.route("/answersTwo",methods=['GET','POST'])  
-def page4():
-    if "answer2" not in session:
-        session["answer2"]=request.form['ques2']
-    return render_template('question3.html')
-@app.route("/answersThree",methods=['GET','POST'])
-def page5():
-    if "answer3" not in session:
-        session["answer3"]=request.form['ques3']
-    return render_template('question4.html')
-@app.route("/answersFour",methods=['GET','POST'])
-def page6():
-    if "answer4" not in session:
-        session["answer4"]=request.form['ques4']
-    session["end_time"] = time.time()
-    time_taken = session["end_time"] - session["start_time"]
-    finalScore=get_score()
-    return render_template('results.html', grade=finalScore, time_taken=time_taken)
 
-
-
-def get_score():
-    scoreVal=0
-    if session["answer1"]=="256":
-        scoreVal+=1
-    if session["answer2"]=="2":
-        scoreVal+=1
-    if session["answer3"]=="20":
-        scoreVal+=1
-    if session["answer4"]=="612":
-        scoreVal+=1
-    return scoreVal
     
 if __name__ == '__main__':
     app.run(debug=False)
